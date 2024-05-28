4aec0ea439fea1775e53c877b8a90307b131c436
NomsuBen <olaniranbenjamin@gmail.com>
2024-05-28 13:02:18 -0400

    Task 3 and 4 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,18 +25,45 @@ public_users.get('/isbn/:id', function (req, res) {
       res.status(404).json({ message: "Book not found" });
     }
   });
-  
-// Get book details based on author
-public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
-});
 
-// Get all books based on title
-public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+// Get book details based on author
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  const matchingBooks = [];
+
+  // Iterate through the books and find books by the author
+  for (const key in books) {
+    if (books[key].author === author) {
+      matchingBooks.push(books[key]);
+    }
+  }
+
+  // Check if any books were found
+  if (matchingBooks.length > 0) {
+    res.json(matchingBooks);
+  } else {
+    res.status(404).json({ message: "No books found by this author" });
+  }
 });
+// Get all books based on title
+public_users.get('/title/:title', function (req, res) {
+    const title = req.params.title;
+    const matchingBooks = [];
+  
+    // Iterate through the books and find books by the title
+    for (const key in books) {
+      if (books[key].title === title) {
+        matchingBooks.push(books[key]);
+      }
+    }
+  
+    // Check if any books were found
+    if (matchingBooks.length > 0) {
+      res.json(matchingBooks);
+    } else {
+      res.status(404).json({ message: "No books found with this title" });
+    }
+  });
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
