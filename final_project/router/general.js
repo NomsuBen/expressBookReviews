bc15e9910385ed091c678ef86b2018c271b4df24
NomsuBen <olaniranbenjamin@gmail.com>
2024-05-28 12:10:53 -0400

    Task Retrieve the ISBN from the request parameters

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -15,11 +15,17 @@ public_users.get("/", function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
-});
-
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+  
+    // Check if the book exists
+    if (books[isbn]) {
+      res.json(books[isbn]);
+    } else {
+      res.status(404).json({ message: "Book not found" });
+    }
+  });
+  
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
   //Write your code here
